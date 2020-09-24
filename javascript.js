e9e6e75365b8b86daaee4a10f4b1521dd9be9445
jasonwillyan <jasonwillyan@hotmail.com>
2020-09-24 18:57:39 -0300

    mod3

--- javascript.js
+++ javascript.js
@@ -4,10 +4,10 @@ $(document).ready(function(){
 
     var btntop = $('#top');
     var btnbase = $('#base');
-    
     var scroll = '';
+    localStorage.clear();
     
-    $('.btnNavegacao').click(function() {
+    $('.btnNavegacao').click(function(){
         var btnId = $(this).attr('id');
         console.log(btnId);
     
@@ -16,11 +16,23 @@ $(document).ready(function(){
         }else if(btnId == "baixo"){
             scroll = btnbase;
         }
-    
+        
         $([document.documentElement, document.body]).animate({
             scrollTop: $(scroll).offset().top
         }, 2000);
 
-    });    
+    });
+    
+    $('#irAA').click(function(){
+        localStorage.setItem("nomecompleto", $('#idnomecompleto').val());
+        localStorage.setItem("email", $('#idemail').val());
+        localStorage.setItem("conteudo", $('#idnomeconteudo').val());
+        localStorage.setItem("url", $('#idurl').val());
+    });
+
+    $('.classbtt').click(function(){
+        alert($(this).parent().children('span').text());
+        //Atribuir respotas ao input do id="respostab[i]"
+    });
 
 });
